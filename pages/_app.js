32ad5a5e142c218befdb2fe1afb2f2514ed01a1c
Farzaneh-ghasemi <ghasemik.farzaneh@gmail.com>
2024-11-29 09:45:38 +0100

    Initialize project: set up first Rust crate with structured modules and basic project layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
 import "@/styles/globals.css";
+import Navbar from "@/components/modules/Navbar/Navbar";
+import Sidebar from "@/components/modules/Sidebar/Sidebar";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+
+export default function App({ Component, pageProps }) { 
+
+  
+  return (
+  <div>
+    <Sidebar />
+    <main className="main">
+      <Navbar />
+      <Component {...pageProps} />
+    </main>
+  </div>
+  )
+
 }
---
+++ pages/api/index.js
@@ -0,0 +1,5 @@
+const handler=(req,res)=>{
+    res.json({"message":"wellcome to api"});
+}
+
+export default handler;
